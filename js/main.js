e28200bf0bd5a896195dba935c5ee49e1b99a98f
Ryan Malaspina <rxm8553@rit.edu>
2016-02-13 20:33:06 -0500

    Rename button_injector.js to main.js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var url_path = window.location.pathname;
+
+alert(getFileName());
+
+// Returns true if user is viewing a 
+// file to download, false otherwise
+function isViewingContent(url_path) {
+  // if path contains "viewContent"
+  if (url_path.indexOf("viewContent") > -1) {
+    return true;
+  }
+  return false;
+}
+
+// Return name of file, no extension
+function getFileName() {
+  var nametag = document.getElementsByClassName('vui-heading-1').first();
+  return nametag.innerText;
+}
