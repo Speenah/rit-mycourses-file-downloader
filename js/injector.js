da86e63346509dd370f18ba9143330089bd713f6
Ryan Malaspina <rxm8553@rit.edu>
2016-02-14 11:40:00 -0500

    Wrote function to add required script to page for dropbox saver

---
+++ js/injector.js
@@ -0,0 +1,3 @@
+function addDropboxScriptTag() {
+  $("head").append('<script type="text/javascript" src="https://www.dropbox.com/static/api/2/dropins.js" id="dropboxjs" data-app-key="8oj6dw6o6urk6wn"></script>');
+}
