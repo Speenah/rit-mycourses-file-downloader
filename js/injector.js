704cae5663ee0db54dc614a18de1a33085244799
Ryan Malaspina <rxm8553@rit.edu>
2016-02-14 11:43:37 -0500

    Grouping comments

--- js/injector.js
+++ js/injector.js
@@ -1,3 +1,7 @@
+/////////////////////////////////
+// Dropbox injection functions //
+/////////////////////////////////
+
 function addDropboxScriptTag() {
   $("head").append('<script type="text/javascript" src="https://www.dropbox.com/static/api/2/dropins.js" id="dropboxjs" data-app-key="8oj6dw6o6urk6wn"></script>');
 }
