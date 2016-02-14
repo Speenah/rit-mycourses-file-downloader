9b3f2bff7a8196d0bf6a00954c6193c51ed376ee
Ryan Malaspina <rxm8553@rit.edu>
2016-02-13 20:21:49 -0500

    button_injector can determine if viewing a file to download

--- js/button_injector.js
+++ js/button_injector.js
@@ -1,3 +1,13 @@
 var url_path = window.location.pathname;
 
-alert(url_path);
+alert(isViewingContent(url_path));
+
+// Returns true if user isviewing a 
+// file to download, false otherwise
+function isViewingContent(url_path) {
+  // if path contains "viewContent"
+  if (url_path.indexOf("viewContent") > -1) {
+    return true;
+  }
+  return false;
+}
