01100bb3c535677d8c36c81ae16b9f44c182d4ad
Ryan Malaspina <rxm8553@rit.edu>
2016-02-15 15:44:29 -0500

    Change cookies from array to object

--- js/background.js
+++ js/background.js
@@ -2,12 +2,12 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.want == "cookies") {
       chrome.cookies.getAll({}, function(cookieList) {
-        var cookies = [];
+        var cookies = {};
         var wanted_cookies = ["d2lSessionVal", "d2lSecureSessionVal"];
         
         for (var i in cookieList) {
           if (wanted_cookies.indexOf(cookieList[i].name) > -1) {
-            cookies.push({name: cookieList[i].name, val: cookieList[i].value});
+            cookies[cookieList[i].name] = cookieList[i].value;
           }
         }
         
